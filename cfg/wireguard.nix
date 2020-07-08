a06e520e870ffdb45d17c87e0a62be7a2e5f4b87
cpli <gwimm@tutanota.com>
2020-07-08 08:56:49 +0800

    experiments: wireguard

--- cfg/wireguard.nix
+++ cfg/wireguard.nix
@@ -5,7 +5,6 @@
 let
   listenPort = 51820;
   externalInterface = "wlan0";
-  source = "10.100.0.0/24";
 
 in
 
@@ -21,31 +20,33 @@ in
 
     wireguard.interfaces = {
       "wg0" = {
-        ips = [ "10.100.0.1/24" ];
+        ips = [ "10.100.0.0/24" ];
         listenPort = listenPort;
 
         postSetup = ''
-          ${pkgs.iptables}/bin/iptables           \
-            --table  nat                          \
-            --append POSTROUTING                  \
-            --source ${source}                    \
-            --jump   MASQUERADE                   \
-            --out-interface ${externalInterface}
+          ${pkgs.iptables}/bin/iptables -A FORWARD -i %i -j ACCEPT
+          ${pkgs.iptables}/bin/iptables -A FORWARD -o %i -j ACCEPT
+          ${pkgs.iptables}/bin/iptables \
+            -t nat                      \
+            -A POSTROUTING              \
+            -o ${externalInterface}     \
+            -j MASQUERADE               \
         '';
 
         postShutdown = ''
-          ${pkgs.iptables}/bin/iptables           \
-            --table  nat                          \
-            --delete POSTROUTING                  \
-            --source ${source}                    \
-            --jump   MASQUERADE                   \
-            --out-interface ${externalInterface}
+          ${pkgs.iptables}/bin/iptables -D FORWARD -i %i -j ACCEPT
+          ${pkgs.iptables}/bin/iptables -D FORWARD -o %i -j ACCEPT
+          ${pkgs.iptables}/bin/iptables \
+            -t nat                      \
+            -D POSTROUTING              \
+            -o ${externalInterface}     \
+            -j MASQUERADE               \
         '';
 
         privateKeyFile = "/srv/wg/prv.b64";
 
         peers = [ { # cpli
-            allowedIPs = [ "10.100.0.2/32" ];
+            allowedIPs = [ "0.0.0.0/0" ];
             publicKey = "o1oXp9AfJW9anC8WNZU+01VhGrtbq4vazmwRPTil03E=";
         } ];
       };
