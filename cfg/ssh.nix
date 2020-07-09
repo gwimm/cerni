97bcf67a37338020f6ddbc1f1bdda47b25cafb5c
cpli <gwimm@tutanota.com>
2020-07-10 05:42:33 +0800

    potential fix: dropbear

--- cfg/ssh.nix
+++ cfg/ssh.nix
@@ -1,15 +1,26 @@
 { config, pkgs, lib, ... }:
 
+let
+  prefix = "/srv/ssh";
+
+in
+
 {
-  # results in:
-  #  ssh-keygen: command not found
-  #  builder for 'sshd.conf-validated.drv' failed with exit code 127
-  # programs.ssh.package = pkgs.dropbear;
+  programs.ssh.package = pkgs.dropbear;
+
+  networking.firewall.allowedTCPPorts = [ 22 ];
 
   services.openssh = {
     enable = true;
 
+    # default key generation fails under dropbear:
+    #  ssh-keygen: command not found
+    #  builder for 'sshd.conf-validated.drv' failed with exit code 127
+    hostKeys = [];
+
     # doesn't work without root password, which can't be set...
     permitRootLogin = "yes";
+
+    authorizedKeyFiles = builtins.attrNames (builtins.readDir "${prefix}/keys")
   };
 }
