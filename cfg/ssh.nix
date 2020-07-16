c9da3fe453c01e45d9f0a581667140a90b50fa72
cpli <gwimm@tutanota.com>
2020-07-17 04:03:06 +0800

    general additions and refactor

--- cfg/ssh.nix
+++ cfg/ssh.nix
@@ -1,11 +1,20 @@
 { config, pkgs, lib, ... }:
 
+# simple SSHD configuration
+
+# TODO
+# find way to limit to only dropbear
+# currently fails at:
+
+# ```
+# ssh-keygen: command not found
+# builder for 'sshd.conf-validated.drv' failed with exit code 127
+# ```
+
 let
   prefix = "/srv/ssh";
 
-in
-
-{
+in {
   programs.ssh.package = pkgs.dropbear;
 
   networking.firewall.allowedTCPPorts = [ 22 ];
@@ -14,13 +23,11 @@ in
     enable = true;
 
     # default key generation fails under dropbear:
-    #  ssh-keygen: command not found
-    #  builder for 'sshd.conf-validated.drv' failed with exit code 127
     hostKeys = [];
 
-    # doesn't work without root password, which can't be set...
+    # disable NixOS insecure default of `true`
+    # passwordAuthentication = false;
     permitRootLogin = "yes";
-
     authorizedKeysFiles =
       builtins.attrNames (builtins.readDir "${prefix}/keys");
   };
